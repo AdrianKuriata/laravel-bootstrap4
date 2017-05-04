74722f52d827fba530d0e840da535ddc19c5d403
austenc <austen.cameron@gmail.com>
2017-05-04 01:22:53 -0600

    updated markup to bootstrap 4 navbar and cards instead of panels

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="card">
+                <div class="card-header">Reset Password</div>
 
-                <div class="panel-body">
+                <div class="card-block">
                     @if (session('status'))
                         <div class="alert alert-success">
                             {{ session('status') }}
